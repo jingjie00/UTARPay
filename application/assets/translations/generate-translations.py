64e78c6931e119993257be94d07e2887b00d6dc8
jingjietan <2000tanjingjie@gmail.com>
2024-11-21 23:32:21 +0800

    Changes to be committed:

--- application/assets/translations/generate-translations.py
+++ application/assets/translations/generate-translations.py
@@ -3,28 +3,26 @@ import json
 import os
 import requests
 
-dir_path = os.path.dirname(os.path.realpath(__file__)) + "\\"
-
 csv_file_name = 'translations.csv'
 
-google_sheets_url = 'https://docs.google.com/spreadsheets/d/1QQqt28cmrby6JqxLm-oxUXCuM3alniLJ6IRhcPJDOtk/gviz/tq?tqx=out:csv'
+# google_sheets_url = 'https://docs.google.com/spreadsheets/d/1QQqt28cmrby6JqxLm-oxUXCuM3alniLJ6IRhcPJDOtk/gviz/tq?tqx=out:csv'
 
-print("Downloading CSV...")
+# print("Downloading CSV...")
 
-response = requests.get(google_sheets_url)
+# response = requests.get(google_sheets_url)
 
-if response.status_code == 200:
-    with open(dir_path + csv_file_name, 'wb') as csv_file:
-        csv_file.write(response.content)
-    print('CSV file downloaded successfully')
-else:
-    print('Failed to download CSV - status code: ', response.status_code)
+# if response.status_code == 200:
+#     with open(csv_file_name, 'wb') as csv_file:
+#         csv_file.write(response.content)
+#     print('CSV file downloaded successfully')
+# else:
+#     print('Failed to download CSV - status code: ', response.status_code)
 
-print("Reading", dir_path + csv_file_name)
+print("Reading", csv_file_name)
 
 
 # Read the CSV file
-with open(dir_path + csv_file_name, 'r', encoding='utf-8') as file:
+with open(csv_file_name, 'r', encoding='utf-8') as file:
     reader = csv.reader(file)
     rows = list(reader)
 
@@ -40,7 +38,7 @@ for lang in languages:
         current_lang_data[row[0]] = row[current_lang_index]
 
     # Write the JSON file
-    with open(dir_path + "generated/" + lang + ".json", 'w', encoding='utf-8') as file:
+    with open("generated/" + lang + ".json", 'w', encoding='utf-8') as file:
         json.dump(current_lang_data, file, indent=2, ensure_ascii=False)
 
     current_lang_index += 1
